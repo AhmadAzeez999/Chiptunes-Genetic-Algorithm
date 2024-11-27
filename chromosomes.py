62e7f916a724a9e853fc131c2ffb76173ed44d93
Ahmad Azeez <114791071+AhmadAzeez999@users.noreply.github.com>
2024-11-27 13:53:46 -0800

    Added 2D array instead

--- chromosomes.py
+++ chromosomes.py
@@ -1,12 +1,12 @@
 import random 
-from pydub import AudioSegment
-from pydub.playback import play
+from typing import List, Tuple
 
 class Chromosome:
     # Initializing a chromosome with random notes, range C4 to B4
-    def __init__(self, targetMelody, numOfNotes, noteRange=(60,72), bestGenes=None):
+    def __init__(self, targetMelody, numOfRows, numOfCols, noteRange=(60,72), bestGenes=None):
         self.noteRange = noteRange
-        self.numOfNotes = numOfNotes
+        self.numOfRows = numOfRows # Number of rows in the 2D array
+        self.numOfCols = numOfCols # Number of columns
         self.targetMelody = targetMelody
 
         if bestGenes is not None:
@@ -18,25 +18,39 @@ class Chromosome:
         self.calculateFitness()
 
     def initializeGenes(self):
-        return [random.randint(self.noteRange[0], self.noteRange[1]) for _ in range(self.numOfNotes)]
+        return [random.randint(self.noteRange[0], self.noteRange[1]) for _ in range(self.numOfCols)
+                for _ in range(self.numOfRows)]
     
     def mutate(self, mutationRate=0.1):
         if random.random() < mutationRate:
-            mutationPoint = random.randint(0, self.numOfNotes - 1)
-            self.genes[mutationPoint] = random.randint(self.noteRange[0], self.noteRange[1])
+            randomRow = random.randint(0, self.numOfRows - 1)
+            randomCol = random.randint(0, self.numOfCols - 1)
+            self.genes[randomRow][randomCol] = random.randint(self.noteRange[0], self.noteRange[1])
             self.fitnessScore = self.calculateFitness()
 
     def crossover(self, mate):
-        crossoverPoint = random.randint(1, self.numOfNotes - 1)
-        childGenes = self.genes[:crossoverPoint] + mate.genes[crossoverPoint:]
-        child = Chromosome(self.targetMelody, self.numOfNotes, self.noteRange)
+        crossoverRow = random.randint(1, self.numOfNotes - 1)
+        crossoverCol = random.randint(1, self.numOfCols - 1)
+        
+        childGenes = [
+            self.genes[row][:crossoverCol] + mate.genes[row][crossoverCol:]
+
+            if row == crossoverRow else self.genes[row]
+            for row in range(self.numOfRows)
+        ]
+
+        child = Chromosome(self.targetMelody, self.numOfRows, self.numOfCols, self.noteRange)
         child.genes = childGenes
         
         return child
     
     def calculateFitness(self):
         # targetMelody will be an array of MIDI notes representing the target melody
-        self.fitnessScore = sum(1 for i in range(self.numOfNotes) if self.genes[i] == self.targetMelody[i])
+        self.fitnessScore = sum(
+            1 for i in range(self.numOfRows)
+            for j in range(self.numOfCols)
+            if self.genes[i][j] == self.targetMelody[i][j]
+        )
 
         return self.fitnessScore
     
